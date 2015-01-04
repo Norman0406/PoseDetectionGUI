b387932de5adb67518907d8a7b4d06a6f656684e
Norman Link <norman.link@gmx.net>
2015-01-04 16:49:12 +0100

    more API infrastructure

--- posegui/src/main2.cpp
+++ posegui/src/main2.cpp
@@ -5,11 +5,33 @@ using namespace std;
 
 void main()
 {
-    IPoseContext* context = 0;
-    if (PoseInit(&context) || context == 0)
+    int width = 512;
+    int height = 414;
+
+    PoseContext* context = 0;
+    if (poseInit(&context, width, height) != RESULT_SUCCESS || context == 0)
         return;
 
+    int depthDataSize = width * height;
+    float* depthData = new float[depthDataSize];
 
+    int pointsDataSize = width * height * 3;
+    float* pointsData = new float[pointsDataSize];
 
-    PoseShutdown(context);
+    // set input data and process data
+    if (poseSetInput(context, depthData, depthDataSize * sizeof(float), pointsData, pointsDataSize * sizeof(float)) != RESULT_SUCCESS)
+        return;
+
+    // get data
+    PoseScene* scene = 0;
+    if (poseGetScene(context, &scene) != RESULT_SUCCESS || scene == 0)
+        return;
+
+    if (poseFreeScene(scene) != RESULT_SUCCESS)
+        return;
+
+    delete[] depthData;
+    delete[] pointsData;
+
+    poseShutdown(context);
 }
